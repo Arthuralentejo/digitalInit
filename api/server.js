239d7a740602f6987cbaaa259f794d6d40e6ea2b
Arthuralentejo <arthur.alentejo228@gmail.com>
2021-10-01 02:21:10 -0300

    Upando primeira api

--- api/server.js
+++ api/server.js
@@ -3,11 +3,15 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
+
     let state = req.query.state
     let dateStart = req.query.dateStart
     let dateEnd = req.query.dateEnd
-    console.log(`${state} --- ${dateStart} ----- ${dateEnd}`)
-    res.end(`${state} --- ${dateStart} ----- ${dateEnd}`)
+    res.send({
+        state: req.query.state,
+        dateStart: req.query.dateStart,
+        dateEnd: req.query.dateEnd
+    })
 })
 
 app.listen(process.env.PORT || 5000, () => {
